454a59d87d0afa7b4d30e5df96f8f40c0ed4b772
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2022-04-14 05:26:10 +0300

    4.3. СОЗДАНИЕ ШАБЛОНА

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ var router = express.Router();
 
 /* Страница Эрена */
 router.get('/eren', function(req, res, next) {
-  res.send("<h1>Страница Эрена</h1>")
+  res.render('hero', {
+    title: "Эрен Йегер",
+    picture: "https://i.pinimg.com/564x/f4/6d/01/f46d01481d2d6148ddbb56c4fd10e0b6.jpg",
+    desc: "Разведчик, титан-прародитель"
+    });
 });
 
 /* Страница Ято */
 router.get('/yato', function(req, res, next) {
-  res.send("<h1>Страница Ято</h1>")
+  res.render('hero', {
+    title: "Ято",
+    picture: "https://i.pinimg.com/564x/4b/dc/58/4bdc58127e7e66f3efdf1a9605f6a967.jpg",
+    desc: "Практически забытый бог погибели, без собственного храма"
+    });
 });
 
 /* Страница Дазая */
 router.get('/dazai', function(req, res, next) {
-  res.send("<h1>Страница Дазая</h1>")
+  res.render('hero', {
+    title: "Дазай",
+    picture: "https://i.pinimg.com/564x/d6/78/01/d678010c43b843c06849cf89749f0ddd.jpg",
+    desc: "Один из старших сотрудников Вооружённого Детективного Агентства"
+    });
 });
 
 module.exports = router;
