aacad0a3607863f086c818493cfe56d50cb554b4
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2022-04-15 03:35:15 +0300

    4.4. УСТАНОВКА И ПОДКЛЮЧЕНИЕ ШАБЛОНИЗАТОРА EJS-LOCALS

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ router.get('/eren', function(req, res, next) {
   res.render('hero', {
     title: "Эрен Йегер",
     picture: "https://i.pinimg.com/564x/f4/6d/01/f46d01481d2d6148ddbb56c4fd10e0b6.jpg",
-    desc: "Разведчик, титан-прародитель"
+    desc: "Член Разведкорпуса, 104-й отряд. Титан-прародитель, атакующий титан, титан-молот"
     });
 });
 
 /* Страница Ято */
 router.get('/yato', function(req, res, next) {
   res.render('hero', {
-    title: "Ято",
+    title: "Бездомный бог Ято",
     picture: "https://i.pinimg.com/564x/4b/dc/58/4bdc58127e7e66f3efdf1a9605f6a967.jpg",
     desc: "Практически забытый бог погибели, без собственного храма"
     });
@@ -22,7 +22,7 @@ router.get('/yato', function(req, res, next) {
 /* Страница Дазая */
 router.get('/dazai', function(req, res, next) {
   res.render('hero', {
-    title: "Дазай",
+    title: "Дазай Осаму",
     picture: "https://i.pinimg.com/564x/d6/78/01/d678010c43b843c06849cf89749f0ddd.jpg",
     desc: "Один из старших сотрудников Вооружённого Детективного Агентства"
     });
