7c5ffee3e48a3f0a4856f00b62c84f69f587aa3b
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2022-06-24 22:46:34 +0300

    8.4 ЗАПОЛНЕНИЕ БАЗЫ ДАННЫХ С MONGOOSE

--- createDB.js
+++ createDB.js
@@ -1,13 +1,47 @@
+var async = require("async")
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
 var Hero = require("./models/hero").Hero
 
-var hero = new Hero({
-    title: "Эрен",
-    nick: "Eren"
-})
+mongoose.connection.on("open", function() {
+    var db = mongoose.connection.db
+    db.dropDatabase(function(err) {
+        if (err) throw err
 
-console.log(hero)
-hero.save(function(err, hero, affected) {
-    console.log(hero.title)
+        async.parallel([
+                function(callback) {
+                    var eren = new Hero({ nick: 'Eren' })
+                    eren.save(function(err, eren) {
+                        callback(err, "Eren")
+                    })
+                },
+                function(callback) {
+                    var yato = new Hero({ nick: 'Yato' })
+                    yato.save(function(err, yato) {
+                        callback(err, "Yato")
+                    })
+                },
+                function(callback) {
+                    var dazai = new Hero({ nick: 'Dazai' })
+                    dazai.save(function(err, dazai) {
+                        callback(err, "Dazai")
+                    })
+                },
+                function(callback) {
+                    var shinji = new Hero({ nick: 'Shinji' })
+                    shinji.save(function(err, shinji) {
+                        callback(err, "Shinji")
+                    })
+                }
+            ],
+            function(err, result) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Успешно созданы герои с никами: " + result.join(", "))
+                }
+                mongoose.disconnect()
+            })
+    })
 })
--- data.js
+++ data.js
@@ -1,20 +1,26 @@
 var data = [{
-    title: 'Эрен',
-    nick: 'Eren',
-    avatar: 'https://i.pinimg.com/564x/f4/6d/01/f46d01481d2d6148ddbb56c4fd10e0b6.jpg',
-    desc: 'Член Разведкорпуса, 104-й отряд. Титан-прародитель, атакующий титан, титан-молот'
-},
-{
-    title: 'Бездомный бог Ято',
-    nick: 'Yato',
-    avatar: 'https://i.pinimg.com/564x/4b/dc/58/4bdc58127e7e66f3efdf1a9605f6a967.jpg',
-    desc: 'Практически забытый бог погибели, без собственного храма'
-},
-{
-    title: 'Дазай Осаму',
-    nick: 'dazai',
-    avatar: 'https://i.pinimg.com/564x/d6/78/01/d678010c43b843c06849cf89749f0ddd.jpg',
-    desc: 'Один из старших сотрудников Вооружённого Детективного Агентства '
-},
+        title: 'Эрен',
+        nick: 'Eren',
+        avatar: 'https://i.pinimg.com/564x/f4/6d/01/f46d01481d2d6148ddbb56c4fd10e0b6.jpg',
+        desc: 'Член Разведкорпуса, 104-й отряд. Титан-прародитель, атакующий титан, титан-молот'
+    },
+    {
+        title: 'Бездомный бог Ято',
+        nick: 'Yato',
+        avatar: 'https://i.pinimg.com/564x/4b/dc/58/4bdc58127e7e66f3efdf1a9605f6a967.jpg',
+        desc: 'Практически забытый бог погибели, без собственного храма'
+    },
+    {
+        title: 'Дазай Осаму',
+        nick: 'dazai',
+        avatar: 'https://i.pinimg.com/564x/d6/78/01/d678010c43b843c06849cf89749f0ddd.jpg',
+        desc: 'Один из старших сотрудников Вооружённого Детективного Агентства '
+    },
+    {
+        title: 'Синдзи Хирако',
+        nick: 'shinji',
+        avatar: 'https://i.pinimg.com/564x/0e/1a/da/0e1ada342dca92e3b11bd3a8e788f5e4.jpg',
+        desc: 'Вайзард'
+    },
 ];
 module.exports.data = data;
