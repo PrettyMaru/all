139cc1407f3010ddc23fa490c1f8d950df435a09
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2022-06-24 23:09:10 +0300

    8.5. ПРИМЕНИЕ ШАБЛОНА SERIES

--- createDB.js
+++ createDB.js
@@ -4,44 +4,58 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
 var Hero = require("./models/hero").Hero
 
-mongoose.connection.on("open", function() {
-    var db = mongoose.connection.db
-    db.dropDatabase(function(err) {
+async.series([
+        open,
+        dropDatabase,
+        createHeroes,
+        close
+    ],
+    function(err, result) {
         if (err) throw err
-
-        async.parallel([
-                function(callback) {
-                    var eren = new Hero({ nick: 'Eren' })
-                    eren.save(function(err, eren) {
-                        callback(err, "Eren")
-                    })
-                },
-                function(callback) {
-                    var yato = new Hero({ nick: 'Yato' })
-                    yato.save(function(err, yato) {
-                        callback(err, "Yato")
-                    })
-                },
-                function(callback) {
-                    var dazai = new Hero({ nick: 'Dazai' })
-                    dazai.save(function(err, dazai) {
-                        callback(err, "Dazai")
-                    })
-                },
-                function(callback) {
-                    var shinji = new Hero({ nick: 'Shinji' })
-                    shinji.save(function(err, shinji) {
-                        callback(err, "Shinji")
-                    })
-                }
-            ],
-            function(err, result) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log("Успешно созданы герои с никами: " + result.join(", "))
-                }
-                mongoose.disconnect()
-            })
+        console.log("ok")
     })
-})
+
+function open(callback) {
+    mongoose.connection.on("open", callback)
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db
+    db.dropDatabase(callback)
+}
+
+function createHeroes(callback) {
+    async.parallel([
+            function(callback) {
+                var eren = new Hero({ nick: 'Eren' })
+                eren.save(function(err, eren) {
+                    callback(err, "Eren")
+                })
+            },
+            function(callback) {
+                var yato = new Hero({ nick: 'Yato' })
+                yato.save(function(err, yato) {
+                    callback(err, "Yato")
+                })
+            },
+            function(callback) {
+                var dazai = new Hero({ nick: 'Dazai' })
+                dazai.save(function(err, dazai) {
+                    callback(err, "Dazai")
+                })
+            },
+            function(callback) {
+                var shinji = new Hero({ nick: 'Shinji' })
+                shinji.save(function(err, shinji) {
+                    callback(err, "Shinji")
+                })
+            }
+        ],
+        function(err, result) {
+            callback(err)
+        })
+}
+
+function close(callback) {
+    mongoose.disconnect(callback)
+}
